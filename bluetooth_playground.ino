5e893b446a48e61ffb6d9163ca24e2809a300f40
Krzysztof Wojciechowski <krzysztof.wojciechowski@itcraft.pl>
2024-04-06 22:14:40 +0200

    WIP library

--- bluetooth_playground.ino
+++ bluetooth_playground.ino
@@ -3,6 +3,7 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <string.h>
+#include "menu.h"
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
@@ -46,96 +47,6 @@ int button2State = 0;
 int lastButton2State = 0;
 int button3State = 0;
 int lastButton3State = 0;
-const int ITEMS_SIZE = 3;
-
-struct menuPos {
-  std::string name;
-  std::string *dataPt;
-  int dataSize;
-};
-
-const int MENU1_SIZE = 2;
-std::string menu1Data[MENU1_SIZE] = {
-  "Data1", "Data2"
-};
-menuPos menu1 = {"A", &menu1Data[0], MENU1_SIZE};
-
-const int MENU2_SIZE = 4;
-std::string menu2Data[MENU2_SIZE] = {
-  "ABC", "XYZ", "YUI", "QWER"
-};
-menuPos menu2 = {"B", &menu2Data[0], MENU2_SIZE};
-
-const int MENU3_SIZE = 2;
-std::string menu3Data[MENU3_SIZE] = {
-  "453", "98372"
-};
-menuPos menu3 = {"C", &menu3Data[0], MENU3_SIZE};
-
-menuPos *menuPositions[ITEMS_SIZE] = {
-  &menu1,
-  &menu2,
-  &menu3
-};
-
-struct menuState {
-  int menuIndex;
-  int selectedMenuIndex;
-  int isMenuSelected;
-  int menuSize;
-  menuPos **menu;
-};
-
-void wrapIndex(menuState *state);
-void onUpClick(menuState *state);
-void onDownClick(menuState *state);
-void onMainClick(menuState *state);
-std::string getDataByState(menuState *state);
-
-struct menuState menu = {
-  0, 0, false, ITEMS_SIZE, &menuPositions[0]
-};
-
-void onUpClick(menuState *state) {
-  state->menuIndex++;
-  wrapIndex(state);
-}
-
-void onDownClick(menuState *state) {
-  state->menuIndex--;
-  wrapIndex(state);
-}
-
-void wrapIndex(menuState *state) {
-  int size;
-  if(state->isMenuSelected) {
-    size = state->menu[state->selectedMenuIndex]->dataSize;
-  } else {
-    size = state->menuSize;
-  }
-  if(state->menuIndex < 0 ){
-    state->menuIndex = size - 1;
-  }
-  state->menuIndex %= size;
-}
-
-void onMainClick(menuState *state) {
-    state->isMenuSelected = !state->isMenuSelected;
-    if(state->isMenuSelected) {
-      state->selectedMenuIndex = state->menuIndex;
-      state->menuIndex = 0;
-    } else {
-      state->selectedMenuIndex = state->menuIndex;
-    }
-}
-
-std::string getDataByState(menuState *state){
-  if(state->isMenuSelected) {
-    return state->menu[state->selectedMenuIndex]->dataPt[state->menuIndex];
-  } else {
-    return (state->menu)[state->menuIndex]->name;
-  }
-}
 
 void loop() {
   button1State = digitalRead(34);
